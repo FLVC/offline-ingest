c55604618fae94217dd52b66ba947482c5f43352
Randy Fischer <randy.fischer@gmail.com>
2013-04-23 19:59:05 -0400

    collection functions moved back into Ingestor

--- lib/offin/ingestor.rb
+++ lib/offin/ingestor.rb
@@ -1,7 +1,20 @@
 require 'rubydora'
 require 'offin/document-parsers'
 require 'offin/mods'
-require 'offin/collection'
+
+# Extend RI mixins to include itql queries
+
+module Rubydora
+  module ResourceIndex
+    def itql query
+      if CSV.const_defined? :Reader
+        FasterCSV.parse(self.risearch(query, :lang => 'itql'), :headers => true)
+      else
+        CSV.parse(self.risearch(query, :lang => 'itql'), :headers => true)
+      end
+    end
+  end
+end
 
 class Ingestor
 
@@ -69,8 +82,9 @@ class Ingestor
 
   def collections= value
     value.each do |pid|
-      collection = Collection.new(@config, @namespace, pid)  # TODO: move Collection back into here....
-      @fedora_object.memberOfCollection << collection.pid
+      fedora_pid = (pid =~ /^info:fedora/ ? pid : "info:fedora/#{pid}")
+      create_new_collection_if_necessary fedora_pid
+      @fedora_object.memberOfCollection << fedora_pid
     end
   end
 
@@ -107,5 +121,66 @@ class Ingestor
   def ingest
     @fedora_object.save
   end
+
+  # dealing with collections for this object to be ingested into
+
+  def existing_collections
+    query = "select $object $title from <#ri> where ($object <fedora-model:label> $title and $object <fedora-model:hasModel> <info:fedora/islandora:collectionCModel>)"
+    @repository.itql(query).map{ |row| row[0] }
+  end
+
+  def collection_policy_text label
+    str = ''
+    @config.content_models.each do |pid, name|
+      str += "         <content_model name=\"#{name}\" dsid=\"ISLANDORACM\" namespace=\"#{@namespace}\" pid=\"#{pid}\"/>\n"
+    end
+
+    # TODO: is name supposed to get the collection label - what's the convention here?
+
+    return <<-XML.gsub(/^     /, '')
+     <collection_policy xmlns="http://www.islandora.ca" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="#{label}" xsi:schemaLocation="http://www.islandora.ca http://syn.lib.umanitoba.ca/collection_policy.xsd">
+       <content_models>
+         #{str.strip}
+       </content_models>
+       <search_terms/>
+       <staging_area/>
+       <relationship>isMemberOfCollection</relationship>
+     </collection_policy>
+    XML
+  end
+
+
+  def create_new_collection_if_necessary pid
+    label = 'digitool collection: ' + pid.sub(/^info:fedora\//, '').sub(/^.*:/, '')
+    return if existing_collections.include? pid
+
+    collection_object = @repository.create(pid)
+
+    collection_object.memberOfCollection << @config.root_collection
+    collection_object.models << 'info:fedora/islandora:collectionCModel'
+    collection_object.label   = label
+    collection_object.ownerId = @config.owner    # TODO: eventually, we'll want a sanity check here that it exists in drupal. Not necessary for digitool migration
+
+    ds = collection_object.datastreams['TN']
+    ds.dsLabel  = "Thumbnail"
+    ds.content  = File.read(@config.collection_thumbnail_filename)
+    ds.mimeType = 'image/png'
+
+    ds = collection_object.datastreams['COLLECTION_POLICY']
+    ds.dsLabel      = "Collection Policy"
+    ds.content      = collection_policy_text label
+    ds.mimeType     = 'text/xml'
+    ds.controlGroup = 'X'
+
+    collection_object.save
+
+    20.times do
+      sleep 0.5
+      return if existing_collections.include? pid
+    end
+
+    raise "collection #{pid} could not be created."
+  end
+
 
 end # of class Ingestor
