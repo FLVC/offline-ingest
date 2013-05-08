ff644627b5f0b4cf2858981bb4aa4fcca92b318a
Randy Fischer <randy.fischer@gmail.com>
2013-05-08 00:04:07 -0400

    doco

--- lib/offin/errors.rb
+++ lib/offin/errors.rb
@@ -1,7 +1,11 @@
 
-# All our main classes include 'error' and 'warning' methods (arrays
-# of strings), accessors (e.g. 'errors') and setters (e.g. 'error
-# string, string...').
+# All our main classes mix in these error and warning utilities:
+#
+#  errors                       - get the error messages, an array of strings
+#  error string [, string]      - add error message(s) to end of array of strings
+#  errors?                      - boolean, true if there are any error messages
+#
+#  and there are similar warnings, warning, and warnings? methods.
 
 module Errors
 
