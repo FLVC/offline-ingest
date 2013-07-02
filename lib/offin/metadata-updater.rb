653f6b479b1167d55c6ed58c3e75b615f71ffcf5
Randy Fischer <rf@ufl.edu>
2013-07-02 16:07:23 -0400

    remove unused cruft

--- lib/offin/metadata-updater.rb
+++ lib/offin/metadata-updater.rb
@@ -26,21 +26,6 @@ class MetadataUpdater
   def get_label fallback
     @manifest.label || @mods.title || fallback
   end
-
-  # Base classes must over-ride these. These are more for
-  # documentation than expected to be executed, since base classes
-  # would never call the unsupported methods.
-
-  def set_label label
-    raise PackageError, "Updating MODS <titleinfo> not implemented."
-  end
-
-  def set_identifiers *identifiers
-    raise PackageError, "Updating MODS/DC identifiers not implemented."
-  end
-
-  # more of these...
-
 end
 
 
@@ -50,45 +35,6 @@ class DigitoolMetadataUpdater < MetadataUpdater
 
   def initialize manifest, mods
     super(manifest, mods)
-    sanity
-
-  end
-
-  # We assume digitool has a very restricted set of data in the MODS
-  # and manifest.xml files.  This method checks those assumptions.
-
-  def sanity
-
-    # manifest is not expected to have: owner, other_logos, identifiers, or submitting_institution
-    # if present, it would be ignored, so let's carp now
-
-    # ....
-
-    # manifest must have non-empty collections and a owning_institution. It may have a submitting_institution.
-
-    # ....
-
-    # mods must not have an <extension> section....
-
-  end
-
-
-  def set_idenfifiers *identifiers
-    raise "TODO"
-  end
-
-  def set_label label
-    raise "TODO"
-  end
-
-
-  def get_extension
-    return <<-XML.gsub(/^    /, '')
-    <extension xmlns:man="info:/flvc/manifest/v1">
-      <man:owningInstitution>#{@manifest.owning_institution}</man:owningInstitution>
-      <man:submittingInstitution>#{@manifest.submitting_institution || @manifest.owning_institution}</man:submittingInstitution>
-    </extension>
-    XML
   end
 
   def get_owner
