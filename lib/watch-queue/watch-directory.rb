f5681534d8eeedcba42754a54bf9860b7b275ab0
Randy Fischer <rf@ufl.edu>
2014-07-28 14:23:30 -0400

    update TODOs

--- lib/watch-queue/watch-directory.rb
+++ lib/watch-queue/watch-directory.rb
@@ -11,11 +11,8 @@ require 'socket'
 # We assume that the ingest database has been initialized at this
 # point; see new_processing_directory().
 
-# TODO: setup for subclassing into WatchProspectiveDirectory,
-# WatchDigitoolDirectory this will probably be hoisting Resque.enqueue
-# into 'def enqueue ..' and let the subclass do IngestProspectiveJob
-# (currently IngestJob) and IngestDigitoolJob (TBD), presumably
-# subclasses themselves
+# TODO: design bug,  we want to be able to sort a variety of subdirectories across several 'watch directories'.  Pluralize!
+
 
 class BaseWatchDirectory
 
