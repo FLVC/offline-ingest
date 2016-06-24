44c3843dea68779fdc1b112936dd608a616240fb
Randy Fischer <rf@ufl.edu>
2016-06-24 19:32:16 -0400

    first cut at tesseract support in new Image class

--- lib/offin/images.rb
+++ lib/offin/images.rb
@@ -4,7 +4,7 @@ require 'offin/errors'
 require 'open3'
 require 'tempfile'
 require 'fileutils'
-
+require 'timeout'
 
 # Helpful constants for several ingest classes:
 
@@ -28,7 +28,29 @@ module ImageConstants
 
   SUPPORTED_IMAGES = [ GIF, JP2, PNG, JPEG, TIFF, PDF ]
 
-  def ImageConstants.executable_location(name)
+  OCR_CANDIDATES = [    # we'll run 'tesseract --list-langs' to figure out which of these are actually compiled into tesseract.
+
+    { :tesseract =>  'eng',
+      :iso639b   =>  'eng',
+      :name      =>  'English' },
+
+    { :tesseract =>  'fra',
+      :iso639b   =>  'fre',
+      :name      =>  'French' },
+
+    { :tesseract =>  'deu',
+      :iso639b   =>  'ger',
+      :name      =>  'French' },
+
+    { :tesseract =>  'ita',
+      :iso639b   =>  'ita',
+      :name      =>  'Italian' },
+  ]
+
+  TESSERACT_TIMEOUT = 120 # 500   # tesseract can waste a lot of time on certain kinds of images
+
+
+  def self.executable_location(name)
     paths = [ '/usr/local/bin', '/usr/bin' ] + ENV['PATH'].split(':')
     paths.each do |dir|
       bin = File.join(dir, name)
@@ -37,16 +59,17 @@ module ImageConstants
     fail "Can't find '#{name}' on path "  + paths.inspect
   end
 
-  TESSERACT_COMMAND      = ImageConstants.executable_location("tesseract")
-  IDENTIFY_EXECUTABLE    = ImageConstants.executable_location("identify")
-  CONVERT_EXECUTABLE     = ImageConstants.executable_location("convert")
-  PDFTOTEXT_EXECUTABLE   = ImageConstants.executable_location("pdftotext")
-  KAKADU_EXECUTABLE      = ImageConstants.executable_location("kdu_expand")
-  GHOSTSCRIPT_EXECUTABLE = ImageConstants.executable_location("gs")
+  TESSERACT_EXECUTABLE   = self.executable_location("tesseract")
+  IDENTIFY_EXECUTABLE    = self.executable_location("identify")
+  CONVERT_EXECUTABLE     = self.executable_location("convert")
+  PDFTOTEXT_EXECUTABLE   = self.executable_location("pdftotext")
+  KAKADU_EXECUTABLE      = self.executable_location("kdu_expand")
+  GHOSTSCRIPT_EXECUTABLE = self.executable_location("gs")
 
   # special tokens in following templates:
   #
   #  %o   the path to the output file
+  #  %t   the path to the output file, but strip off the extension
   #  %i   the path to the input file
   #  %m   the path to the input file,  '[0]' is appended if input is a TIFF or PDF, which selects first image of potential multi page images.
 
@@ -55,6 +78,8 @@ module ImageConstants
   KAKADU_TO_TIFF_COMMAND = "#{KAKADU_EXECUTABLE} -quiet -i %i -o %o"
   PDF_TO_TEXT_COMMAND = "#{PDFTOTEXT_EXECUTABLE} -nopgbrk %i %o"
   CONVERT_WITH_LZW_COMPRESSION_COMMAND = "#{CONVERT_EXECUTABLE} -quiet -compress LZW %m %o"
+  OCR_COMMAND = "#{TESSERACT_EXECUTABLE} %l %i %t"
+  HOCR_COMMAND = "#{TESSERACT_EXECUTABLE} %l %i %t hocr"
 
   # Experiments that resulted in problematic DPIs in some output formats
   #
@@ -85,21 +110,22 @@ class Image
     @mime_type = get_mime_type
     @file_io = File.open(file_path, 'rb')
     @size = File.stat(@file_path).size
+    @tesseract_languages = supported_ocr_languages
 
-    # GIF conversion routines
+    # Setup GIF conversion routines
 
     THUNKS[GIF][JP2]  = image_to_jp2()
     THUNKS[GIF][PDF]  = convert_with_lzw_compression(PDF)
     THUNKS[GIF][TIFF] = convert_with_lzw_compression(TIFF)
-    THUNKS[GIF][OCR]  = nil
-    THUNKS[GIF][HOCR] = nil
+    THUNKS[GIF][OCR]  = image_ocr
+    THUNKS[GIF][HOCR] = image_hocr
 
     [ GIF, JPEG, PNG ].each { |target_mime_type| THUNKS[GIF][target_mime_type] = convert_to_basic_image(target_mime_type) }
 
     # JP2 conversion routines
 
-    THUNKS[GIF][OCR]  = nil
-    THUNKS[GIF][HOCR] = nil
+    THUNKS[JP2][OCR]  = image_ocr
+    THUNKS[JP2][HOCR] = image_hocr
 
     [ GIF, JP2, JPEG, PDF, PNG, TIFF ].each { |target_mime_type| THUNKS[JP2][target_mime_type] = jp2_to_image(target_mime_type) }
 
@@ -114,8 +140,8 @@ class Image
     THUNKS[TIFF][JP2]  = image_to_jp2
     THUNKS[TIFF][PDF]  = convert_with_lzw_compression(PDF)
     THUNKS[TIFF][TIFF] = convert_with_lzw_compression(TIFF)
-    THUNKS[TIFF][HOCR] = nil
-    THUNKS[TIFF][OCR]  = nil
+    THUNKS[TIFF][HOCR] = image_hocr
+    THUNKS[TIFF][OCR]  = image_ocr
 
     [ GIF, JPEG, PNG ].each { |target_mime_type| THUNKS[TIFF][target_mime_type] = convert_to_basic_image(target_mime_type) }
 
@@ -142,7 +168,7 @@ class Image
   def dimensions
     return @file_dimensions if @file_dimensions
     data = nil
-    errors = nil
+    oops = nil
     Open3.popen3(IDENTIFY_EXECUTABLE, file_path) do |stdin, stdout, stderr|
       stdin.close
       data = stdout.gets
@@ -165,12 +191,12 @@ class Image
   def convert(target_mime_type, geometry=nil)
     proc = THUNKS[mime_type][target_mime_type]
     if proc.nil?
-      error "derivative creation failure for image '#{file_name}' - conversion from '#{mime_type}' to '#{target_mime_type}' isn't supported" if proc.nil?
+      error "derivative creation failed for image '#{file_name}' - conversion from '#{mime_type}' to '#{target_mime_type}' isn't supported" if proc.nil?
       return nil
     end
     return proc.call(geometry)
   rescue => e
-    error "derivative creation failure for image '#{file_name}' - during conversion from '#{mime_type}' to '#{target_mime_type}' the following error occured: ", e.message
+    error "derivative creation failed for image '#{file_name}' - during conversion from '#{mime_type}' to '#{target_mime_type}' the following error occured: ", e.message
     return nil
   end
 
@@ -183,8 +209,8 @@ class Image
     when JP2;    'jp2'
     when PDF;    'pdf'
     when TEXT;   'text'
-    when OCR;    'ocr'
-    when HOCR;   'hocr'
+    when OCR;    'txt'
+    when HOCR;   'html'
     else
       fail "Unexpected mimetype '#{mime_type}' encountered when processing '#{file_name}'"  # only expected to fail for bad file, never on internal use
     end
@@ -206,8 +232,11 @@ class Image
 
   def jp2_to_image(target_mime_type)
     return Proc.new do |geometry|
-      @kakadu_cached_file  ||= run_command(KAKADU_TO_TIFF_COMMAND, TIFF).path # creates uncompressed TIFF
-
+      unless @kakadu_cached_file
+        fd = run_command(KAKADU_TO_TIFF_COMMAND, TIFF) # N.B.  Kakadu creates uncompressed TIFFs
+        fail "could not create a temporary TIFF file from #{file_name}"
+        @kakadu_cached_file = fd.path
+      end
       case target_mime_type
       when TIFF, PDF
         run_command(CONVERT_WITH_LZW_COMPRESSION_COMMAND, target_mime_type, geometry, @kakadu_cached_file)
@@ -215,10 +244,8 @@ class Image
         run_command(CONVERT_TO_JP2_COMMAND, target_mime_type, geometry, @kakadu_cached_file)
       when GIF, PNG, JPEG
         run_command(CONVERT_TO_BASIC_IMAGE_COMMAND, target_mime_type, geometry, @kakadu_cached_file)
-      when TEXT
-        fail "no suppport for JP2 to TEXT (yet)"
       else
-        fail "no suppport for JP2 to #{target_mime_type}"
+        fail "no conversion suppport is available for JP2 to #{target_mime_type}"
       end
     end
   end
@@ -259,6 +286,47 @@ class Image
     end
   end
 
+  def image_hocr
+    image_ocr(:hocr)
+  end
+
+  def image_ocr(hocr = false)
+    return Proc.new do |languages|
+      input_file_name = file_name
+
+      if not [ JPEG, TIFF ].include? mime_type
+        @tesseract_intermediary_cached ||= run_command(CONVERT_WITH_LZW_COMPRESSION_COMMAND, TIFF)
+        fail "unable to create a temporary TIFF file for performing OCR"
+        input_file_name = @tesseract_intermediary_cached.path
+      end
+
+      args = []
+      if languages
+        languages.each do |lang|
+          record = @tesseract_languages.select { |rec| rec[:iso639b] == lang }.shift
+          args.push "-l #{record[:tesseract]}" unless record.nil?
+        end
+      end
+      args = [ "-l eng" ] if args.empty?
+      command = nil
+      begin
+        Timeout.timeout(TESSERACT_TIMEOUT) do
+          if hocr
+            command = HOCR_COMMAND.sub("%l", args.join(" "))
+            run_command(command, HOCR, nil, input_file_name)
+          else
+            command = OCR_COMMAND.sub("%l", args.join(" "))
+            run_command(command, OCR, nil, input_file_name)
+          end
+        end
+      rescue Timeout::Error => e
+        error "The OCR command '#{command}' was taking too long: stopped after #{TESSERACT_TIMEOUT} seconds"
+      rescue => e
+        error "The OCR command '#{command}' failed:", e.message
+      end
+    end
+  end
+
 
   def command_template_substitutions(command_template, input_file_name, output_file_name, output_mime_type, geometry=nil)
     cmd = []
@@ -271,6 +339,8 @@ class Image
         str += '[0]'  if [ TIFF, PDF ].include?(get_mime_type(input_file_name))
       when /%o/
         str.sub!(/%o/, output_file_name)
+      when /%t/
+        str.sub!(/%t/, output_file_name.sub(/\.[^\.]*$/, ''))
       end
       cmd.push str
     end
@@ -278,18 +348,26 @@ class Image
     return cmd
   end
 
+
+  def not_really_errors(oops)
+    return true  if (oops =~ /Tesseract Open Source OCR Engine/i and oops.length < 65)
+    return false
+  end
+
+
   def run(argv)
     STDERR.puts argv.join(' ')
 
     data = nil
-    errors = nil
+    oops = nil
     Open3.popen3(*argv) do |stdin, stdout, stderr|
       stdin.close
       data = stdout.gets
-      errors = stderr.read
+      oops = stderr.read
     end
-
-    fail "Errors when creating derivative: '" + errors.gsub("\n", ";  ") + "'" unless errors.nil? or errors.empty?
+    unless (oops.nil? or oops.empty? or not_really_errors(oops))
+      error "could not create derivative for '#{file_name}'",  "failed command: '#{argv.join(' ')}'", oops.split("\n")
+    end
   end
 
   # create a derivation for this image, on success return an opened file i/o object for the newly created derivative.
@@ -300,10 +378,16 @@ class Image
     cmd = command_template_substitutions(command_template, input_file_name, output_file_name, output_mime_type, geometry)
     run(cmd)
 
-    fail "Derivative creation failed" unless File.exists?(output_file_name)
-    fail "Derivative creation failed - empty file"  unless File.stat(output_file_name).size > 0
+    unless File.exists?(output_file_name)
+      error "failed derivative creation of '#{output_mime_type}' for '#{file_name}'"
+      return nil
+    end
 
     return open(output_file_name, 'rb')
+
+  rescue => e
+      error "failed derivative creation of '#{output_mime_type}' for '#{file_name}'", e.message
+      return nil
   end
 
 
@@ -315,10 +399,10 @@ class Image
   def get_mime_type(path=nil)
     path ||= file_path
     type  = nil
-    error = nil
+    oops = nil
     Open3.popen3("/usr/bin/file",  "--mime-type",  "-b", path) do |stdin, stdout, stderr|
-      type   = stdout.read
-      error  = stderr.read
+      type  = stdout.read
+      oops  = stderr.read
     end
     type.strip!
     type = 'image/jp2'    if (path =~ /\.jp2/i and type == 'application/octet-stream')
@@ -341,6 +425,27 @@ class Image
     return name
   end
 
+  def supported_ocr_languages
+    tesseract_languages = []
+    text = ''
+    Open3.popen3(TESSERACT_EXECUTABLE, '--list-langs') do |stdin, stdout, stderr|
+      stdin.close
+      stdout.close
+      text = stderr.read
+    end
+
+    text.split("\n").each do |lang|
+      lang.strip!
+      tesseract_languages += OCR_CANDIDATES.select { |rec| rec[:tesseract] == lang }
+    end
+
+  rescue => e
+    error "Can't determine supported OCR languages", e.message
+    return tesseract_languages
+  end
+
+
+
 end # of image class
 
 include ImageConstants
@@ -351,16 +456,36 @@ Kernel.trap('PIPE') { STDERR.puts "Pipe Closed"  ; exit }
 
 Image.new(input_file_name = ARGV[0]) do |image|
   puts "input #{image.mime_type} image #{image.file_name} size: #{image.size}"
+  puts image.errors  if image.errors?
+  puts image.warnings  if image.warnings?
+  puts image.notes  if image.notes?
 
-  [ TEXT, GIF, JP2, PNG, JPEG, OCR, TIFF, PDF, HOCR ].each do |target|
+  # [ TEXT, GIF, JP2, PNG, JPEG, TIFF, PDF ].each do |target|
+  #     begin
+  #       image.reset_errors
+  #       name = "test-from-" + image.file_name_label + "-" + image.extension(image.mime_type) + "-to." + image.extension(target)
+  #       fd = image.convert(target)
+  #       STDERR.puts image.errors  if image.errors?
+  #       fd.nil? && next
+  #       open(name, 'w') do |out|
+  #         while (data = fd.read(1024 * 1024))
+  #           out.write data
+  #         end
+  #       end
+  #     rescue => e
+  #       puts "output #{target} image #{name} error: #{e.message}"
+  #     end
+  # end
+
+  [ OCR, HOCR ].each do |target|
       begin
         image.reset_errors
         name = "test-from-" + image.file_name_label + "-" + image.extension(image.mime_type) + "-to." + image.extension(target)
-        fd = image.convert(target)
-        if fd.nil?
-          STDERR.puts image.errors
-          next
+        fd = image.convert(target, [ 'eng', 'fre' ])
+        if image.errors?
+          STDERR.puts "----------------", image.errors, ''
         end
+        fd.nil? && next
         open(name, 'w') do |out|
           while (data = fd.read(1024 * 1024))
             out.write data
@@ -370,4 +495,6 @@ Image.new(input_file_name = ARGV[0]) do |image|
         puts "output #{target} image #{name} error: #{e.message}"
       end
   end
+
+
 end
